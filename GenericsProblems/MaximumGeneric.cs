03b6b8fe098886a5dbe089333e767c004c58ffe5
somu3341 <somashekarvsv1996@gmail.com>
2023-03-20 13:00:31 +0530

    [somu]Add:Finding Maximum Integer.

---
+++ GenericsProblems/MaximumGeneric.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblems
+{
+    public class MaximumGeneric
+    {
+        public void Print<M>( M a, M b, M c) where M : IComparable
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
+                Console.WriteLine("a is Maximum");
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+                Console.WriteLine("b is Maximum");
+            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0)
+                Console.WriteLine("c is Maximum");
+        }
+    }
+}
---
+++ GenericsProblems/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace GenericsProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaximumGeneric maximumGeneric = new MaximumGeneric();
+            maximumGeneric.Print<int>(1,29,6);
+        }
+    }
+}

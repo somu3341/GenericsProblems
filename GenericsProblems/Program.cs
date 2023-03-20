cabb7173849f83c926f9c080fbfd60b8bac82069
somu3341 <somashekarvsv1996@gmail.com>
2023-03-20 17:09:00 +0530

    [somu]Add:Finding Maximum String.

--- GenericsProblems/Program.cs
+++ GenericsProblems/Program.cs
@@ -3,8 +3,17 @@ using System;
 using System.Globalization;
 using System.IO;
 using System.Security.Cryptography;
+namespace GenericsProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaximumGeneric maximumGeneric = new MaximumGeneric();
+            maximumGeneric.Integer<int>(1, 29, 6);
+            maximumGeneric.Float<float>(29, 2, 5);
+            maximumGeneric.String<string>("Apple", "Peach", "Banana");
+        }
+    }
+}
 
-MaximumGeneric maximumGeneric = new MaximumGeneric();
-maximumGeneric.Integer<int>(1, 29, 6);
-maximumGeneric.Float<float>(29, 2, 5);
-maximumGeneric.String<string>("Apple", "Peach", "Banana");
